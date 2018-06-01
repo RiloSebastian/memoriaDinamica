2846a2e646046fa8d09f1f5720eb0359c87dfee8
s3baRr00 <sebastianrilo@gmail.com>
2018-06-01 10:23:30 -0300

    memoria dinamica 1

---
+++ MemoriaDinamica1.0/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+char nombre[50];
+int id;
+
+}ePersona;
+
+int epersona_setId(ePersona*, int);
+int epersona_setNombre(ePersona*, char*);
+int epersona_getId (ePersona*);
+char* epersona_getnombre (ePersona*);
+int main()
+{
+    ePersona* pPersona;//puntero
+
+
+    pPersona= (ePersona*)/*para saber que tipo de dato recibe el puntero a void*/ malloc(sizeof(ePersona));// malloc reserva una cantidad de bytes consecutivos en memoria para guardar el tipo de dato
+    //dejo de apuntar al stack y ahora apunta al heap.
+    /*es como hacer
+    epersona= &persona
+    */
+
+    if(pPersona!=NULL){ //para asegurarse de que no sea null
+     if(epersona_setId(pPersona,45)==-1){
+      printf("id incorrecto\n");
+     }
+     else{
+      if(epersona_setNombre(pPersona, "marta")==-1){
+       printf("id incorrecto\n");
+      }
+      else{
+       printf("%d--%s", epersona_getId(pPersona),epersona_getnombre(pPersona));
+      }
+     }
+    }
+    return 0;
+}
+
+int epersona_setId(ePersona* this, int id){// 'this': palabra reservada de c++.
+int ret=-1;//dentro de un set nunca se muestra un mensaje
+//por eso el set devuelve un entero
+
+  if(this!= NULL && id>0){
+   this->id = id; // this es un dato tipo persona que tiene su campo id propio.
+   ret=1;
+  }
+ return ret;
+}
+
+
+int epersona_setNombre(ePersona* this, char* nombre){
+int ret=-1;
+
+  if(this!= NULL &&  nombre!= NULL && strlen(this->nombre)>3){
+    strcpy(this->nombre,nombre);
+     ret=1;
+    }
+ return ret;
+
+}
+
+int epersona_getId (ePersona* this){
+int ret=-1;
+
+    if(this!=NULL){
+        ret= this->id;
+    }
+return ret;
+}
+
+char* epersona_getnombre (ePersona* this){
+char* ret = NULL;
+
+ if(this!=NULL){
+  ret = this->nombre;
+ }
+
+return ret;
+}
